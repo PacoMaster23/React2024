c83c9ce0d96b2c30dc546466647863e5ff261487
Francisco Batalla <francizco560@hotmail.com>
2024-10-23 11:31:06 -0600

    Update repo with third project

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,20 +1,73 @@
 import { useEffect, useState } from "react"
 
-function App() {
 
-  const [enable, setEnable] = useState(false)
+ const FollowMouse = ()=>{
+  const [enabled, setEnabled] = useState(false)
+  const [position,setPosition] = useState ({x:0, y:0})
+
   useEffect(() => {
-    console.log('efecto')
-  })
+    console.log('efectt' , {enabled})
+  
 
-  return (
-    <>      <h1>HOLA DESDE REACT</h1>
-      <button >{enable ? 'Activar' :'Desactivar'}</button>
+  const handleMove = (event)  =>{
+    const {clientX, clientY} = event
+  console.log ('handleMove', {clientX, clientY})
+  setPosition ({x:clientX, y:clientY})
+  }
+
+  if ( enabled){
+  window.addEventListener ('pointermove', handleMove)
+
+}
+// cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    return () => { // cleanup method
+      console.log('cleanup')
+  window.removeEventListener('pointermove', handleMove)
+}
+}, [enabled])
 
 
-    </>
+  return(
+   <>
 
+   <div style={{
+        position: 'absolute',
+        backgroundColor:  'rgba(0, 0, 0, 0.5)',
+        borderRadius: '50%',
+        border : '1px solid #fff',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+        }}>
+          </div>
+  
+  
+     <button onClick={()=> setEnabled(!enabled)} >
+        {enabled ? 'Desctivar' :'Activar'} Seguir Puntero
+        </button>
+
+        
+        </> 
   )
+ }
+
+
+ 
+function App() {  
+  const [mounted, setMounted] = useState(true)
+    return (
+      <main>
+   <FollowMouse/>
+      </main>
+  )
+
 }
 
 export default App
