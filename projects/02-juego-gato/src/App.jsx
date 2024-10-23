97c16bba184904e05e99ec1e8219d1a7a69e7cfa
Francisco Batalla <francizco560@hotmail.com>
2024-10-23 09:14:48 -0600

    Extract to files save to storage

--- projects/02-juego-gato/src/App.jsx
+++ projects/02-juego-gato/src/App.jsx
@@ -7,12 +7,25 @@ import { TURNS } from './constans'
 import { checkEndGame, CheckWinnerFrom } from './logic/board'
 import { WinnerModal } from './components/WinnerModal'
 
+import { saveGameStorage, resetGameStorage } from './logic/storage'
 
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() =>{
+    const boardFromStorage = window.localStorage.getItem('board')
+      return boardFromStorage ? JSON.parse(boardFromStorage) : Array (9).fill(null)
+    
+  })
+  
+  
+  
+
+  const [turn, setTurn] = useState ( () =>{
+    const turnFromStorage = window.localStorage.getItem ('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+  
   const [winner, setWinner] = useState (null)
 
   
@@ -20,6 +33,8 @@ function App() {
     setBoard (Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    resetGameStorage()
+
   }
 
 
@@ -37,6 +52,12 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
 
     setTurn(newTurn)
+      // guardar partida
+
+      saveGameStorage({
+        board:newBoard,
+        turn: newTurn
+  })
     // revisar si hay ganador
     const newWinner = CheckWinnerFrom(newBoard)
     if (newWinner){
