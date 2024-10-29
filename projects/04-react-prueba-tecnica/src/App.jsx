1a9a04f954ed01514ad48083c7163be57ba92931
Francisco <francizco560@hotmail.com>
2024-10-29 16:47:42 -0600

    API FUNCIONANDO

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,53 +1,23 @@
-import { useEffect, useState } from "react"
 import './App.css'
-const CAT_ENDOPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-
-
-const CAT_PREFIX_IMAGE_URL ='https://cataas.com'
-
+import { useCatImage } from './hooks/useCatImage.js'
+import { useCatFact } from './hooks/useCatFact.js'
 
 export function App () {
-    const [fact,setFact] = useState ()
-    const [imageUrl, setImageUrl] = useState()
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
 
-    // recuperar la ctia al cargar la pagina
-     useEffect (() =>{
-        fetch (CAT_ENDOPOINT_RANDOM_FACT)
-        .then(res => res.json())
-        .then(data => {
-        const {fact} = data 
-        setFact(fact)
-        
+  const handleClick = async () => {
+    refreshFact()
+  }
 
-        
-        })
-       },[])
+  return (
+    <main>
+      <h1>App de gatitos</h1>
 
-       useEffect (() =>{
-        if (!fact)return
+      <button onClick={handleClick}>Get new fact</button>
 
-        const firstThreeWords
-         = fact.split(' ', 3).join(' ')
-        console.log(firstThreeWords) 
-
-        fetch(`https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&
-         json=true`)
-
-            .then(res =>res.json( ))
-            .then(response => {
-                const {url} = response
-                setImageUrl(url)
-            })
-       }, [fact])
-    
-    return(
-
-        <main>
-        <h1>App de Gatitos</h1>
-        
-        {fact && <p>{fact}</p>}
-        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`image extracted using the 
-            first three words for ${fact}`}/>}
-        </main>
-    )
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+    </main>
+  )
 }
